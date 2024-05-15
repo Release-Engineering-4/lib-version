e883689809998a774503e14e8fc5ff5678fb4a2b
Ming Da Yang <m.d.yang@student.tudelft.nl>
2024-05-15 15:13:06 +0200

    working python package

---
+++ build/lib/libversion/version_util.py
@@ -0,0 +1,38 @@
+import pkg_resources
+import os
+import subprocess
+
+
+class VersionUtil:
+    @staticmethod
+    def get_version():
+        '''
+        Tries to get library version from files. If that fails,
+        gets library version from Git
+        '''
+        version = VersionUtil.get_version_from_file()
+        if not version:
+            version = VersionUtil.get_version_from_git()
+        return version
+
+    @staticmethod
+    def get_version_from_file():
+        '''
+        Get library version from file.
+        '''
+        version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
+        with open(version_file, 'r') as file:
+            version = file.read().strip()
+        return version
+
+    @staticmethod
+    def get_version_from_git():
+        '''
+        Get library version from Git.
+        '''
+        try:
+            tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode().strip()
+            version = tag.lstrip('v')
+        except (subprocess.CalledProcessError, OSError):
+            version = None
+        return version
