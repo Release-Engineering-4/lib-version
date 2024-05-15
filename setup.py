bad2e26a7b0b35a35ca9d1eaa362d5cf88de132e
Ming Da Yang <m.d.yang@student.tudelft.nl>
2024-05-15 14:27:43 +0200

    refactored package so it can be installed using Pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='lib-version',
+    version='0.0.1',
+    description='A simple library to get the version of the library',
+    url='https://github.com/Release-Engineering-4/lib-version',
+    author='Release Engineering 4',
+    author_email='m.d.yang@student.tudelft.nl',
+    packages=['lib_version'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Students',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
